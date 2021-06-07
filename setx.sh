0463eb58d4e5a6d1e65b0f5ee89c5b99a8193791
ZEXOEG <78424887+ZEXOEG@users.noreply.github.com>
2021-06-07 07:08:17 +0200

    Update setx.sh

--- setx.sh
+++ setx.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo "
-          _  __    __  __
-         | |/ /___|  \/  | ___
-         | ' // _ \ |\/| |/ _ \
-         | . \  __/ |  | | (_) |
-         |_|\_\___|_|  |_|\___/
+          ࿗ッ𓆩Zex_o𓆪
 
-        𓍹 Visit @rruuurr for help 𓍻
+        𓍹 Visit @Zex_o for help 𓍻
 "
 
 python3 -m userbot
